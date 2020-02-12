21e348067286351fc614b74ec2e3f361c399cbfd
yanyongchao <yongchao.yan@welian.com>
2020-02-12 16:19:37 +0800

    加eslint

--- src/App.vue
+++ src/App.vue
@@ -10,20 +10,17 @@
 
 <style lang="scss">
 #app {
+  text-align: center;
+  color: #2c3e50;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
-
 #nav {
   padding: 30px;
-
   a {
-    font-weight: bold;
     color: #2c3e50;
-
+    font-weight: bold;
     &.router-link-exact-active {
       color: #42b983;
     }
